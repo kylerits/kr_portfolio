ddcaeaa47267cedfd38864306a234907dbaa64b8
Kyle Rodgers <kyle.rits.rodgers@gmail.com>
2019-09-15 20:04:18 -0400

    Initial Commit

---
+++ gridsome.config.js
@@ -0,0 +1,35 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'devFortuna',
+  plugins: [{
+      use: 'gridsome-plugin-tailwindcss',
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        purgeConfig: {},
+        presetEnvConfig: {},
+        shouldPurge: true,
+        shouldImport: true,
+        shouldTimeTravel: true,
+        shouldPurgeUnusedKeyframes: true,
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        baseDir: './content/portfolio',
+        path: '*.md',
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            create: true
+          }
+        }
+      }
+    }
+  ]
+};
---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout>
+    <h1 class="font-sans text-5xl mb-6 text-gray-500 font-light -ml-6">Coming Soon!</h1>
+
+    <p
+      class="font-serif text-base text-gray-800 mb-10"
+    >I am currently working on rebuilding my website. If you would like to le arn more about me, check me out via the links below! Thank you for your patience.</p>
+
+    <p class="home-links">
+      <a
+        href="https://www.linkedin.com/in/kyleritschardrodgers/"
+        target="_blank"
+        rel="noopener"
+      >LinkedIn</a>
+      <a href="https://github.com/kylerits" target="_blank" rel="noopener">GitHub</a>
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!"
+  }
+};
+</script>
+
+<style>
+.home-links a {
+  @apply mr-6 px-3 py-2 text-blue-700 rounded shadow bg-blue-100;
+  transition: all 300ms;
+}
+.home-links a:hover {
+  @apply bg-blue-200;
+}
+</style>
