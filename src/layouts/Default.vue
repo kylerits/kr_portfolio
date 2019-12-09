403c056302a233990fd55df5d599e95b4de73be3
Kyle Rodgers <kyle.rits.rodgers@gmail.com>
2019-12-09 18:01:01 -0500

    updating schema

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -14,6 +14,26 @@ export default {
   components: {
     KrHeader,
     KrFooter
+  },
+  metaInfo: {
+    script: [
+      {
+        innerHTML: `
+        {
+          "@context": "https://schema.org/",
+          "@type": "Person",
+          "name": "Kyle Rodgers",
+          "url": "https://kylerodgers.me/",
+          "image": "https://kylerodgers.me/assets/static/4k-wallpaper-conifers-desktop-wallpaper-1920x1080.2665e34.f49e64e7789e5328dd0ad2cc37236ed8.jpg",
+          "sameAs": [
+            "https://twitter.com/kyle_rits",
+            "https://www.linkedin.com/in/kyleritschardrodgers/"
+          ],
+          "jobTitle": "Web Developer"  
+        }`,
+        type: "application/ld+json"
+      }
+    ]
   }
 };
 </script>
